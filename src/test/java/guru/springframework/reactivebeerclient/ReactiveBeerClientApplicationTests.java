d5a31db4b7f2c3c800abe0d968fd7e721b0f3de8
Jorge Cue <jorge.cue@gmail.com>
2021-08-08 21:39:21 -0500

    infrastructure: Initial commit

---
+++ src/test/java/guru/springframework/reactivebeerclient/ReactiveBeerClientApplicationTests.java
@@ -0,0 +1,18 @@
+package guru.springframework.reactivebeerclient;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+class ReactiveBeerClientApplicationTests {
+
+    @Test
+    @Disabled("Test without assertions")
+    void contextLoads() {
+        fail("Test without assertions");
+    }
+
+}
